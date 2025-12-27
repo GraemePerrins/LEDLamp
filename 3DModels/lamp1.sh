6e2946645e1287c174927bdd7ee05a623a70df9c
Rogue Cyclist <42662431+GraemePerrins@users.noreply.github.com>
2025-12-27 17:39:56 +1100

    Delete 3DModels/lamp1.sh

--- 3DModels/lamp1.sh
+++
@@ -1,2 +0,0 @@
-python lampGen.py --outer-diameter 100 --height 180 --wall-thickness 1.1 --cap-thickness 3 --plain-bottom 15 --plain-top 5  --num-turns 6 --num-slots 5 --slot-inset 3.0 --slot-arc-length 20 --slot-radial-width 3.0  --ridge-width 10 --ridge-height 1 --edge-smooth-ratio 0.15 --segments 260 --output lamp3.stl
-
